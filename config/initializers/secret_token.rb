a5a6f7f427a86c2322d9ab578633a7fe90734bfd
Madis Nõmme <madis.nomme@gmail.com>
2013-04-27 15:18:26 +0300

    Added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdsMatcher::Application.config.secret_token = 'f01f9157f54d0b570f7bd57b549e1579697e194b2d4106dc7cd7a0c112f1ccea0d07966db782e589915ba3a86c047abe4bcbf1dcf84d50924c727921cc039597'

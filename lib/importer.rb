26c8a14c58518f724851aeaedd9e2f701d6ea52e
Karl Kilgi <kilgi.karl@gmail.com>
2013-04-29 00:57:49 +0300

    Importing data into AdsEntry from csv.

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,5 +1,18 @@
+require 'active_support/core_ext/hash/slice'
 class Importer
-  def import
-    p "IMport ftw!"
+  def import (filename)
+    p 'Import ftw!'
+    CSV.foreach(Rails.root.join('data', filename), :quote_char => '"', :headers => true, :col_sep => ';')  do |row|
+
+      selected_values = row.to_hash.select{|k,v| ['ADOB_ID', 'ADR_ID', 'ADS_KEHTIV', 'ADS_OID', 'LAHIAADRESS', 'OLED', 'ORIG_TUNNUS', 'TAISAADRESS', 'VIITEPUNKT_X', 'VIITEPUNKT_Y'].include?(k)}
+
+      lower_case_hash = {}
+      selected_values.each_pair do |k,v|
+        lower_case_hash.merge!({k.downcase => v})
+      end
+
+      AdsEntry.create!(lower_case_hash)
+    end
+    p 'Import done!'
   end
 end
--- lib/tasks/import_data.rake
+++ lib/tasks/import_data.rake
@@ -1,7 +1,10 @@
-namespace :adb do
-  desc "Import data from CSV to database"
-  task :import_data do
-    puts "Importing..."
-  end
+require 'csv'
 
+namespace :adb do
+  desc 'Import data from CSV to database'
+  #For example rake adb:import_data[136_rows.csv]
+  task :import_data, [:filename] => :environment do |t, args|
+    puts 'Importing...'
+    Importer.new.import (args.filename)
+  end
 end

6907036214e58888631501e129d9540b31496bf1
Karl Kilgi <kilgi.karl@gmail.com>
2013-04-29 09:21:06 +0300

    Nicer look.

--- lib/importer.rb
+++ lib/importer.rb
@@ -2,16 +2,11 @@ require 'active_support/core_ext/hash/slice'
 class Importer
   def import (filename)
     p 'Import ftw!'
-    CSV.foreach(Rails.root.join('data', filename), :quote_char => '"', :headers => true, :col_sep => ';')  do |row|
+    CSV.foreach(Rails.root.join('data', filename), :quote_char => '"', :headers => true,  :header_converters => :symbol, :col_sep => ';')  do |row|
 
-      selected_values = row.to_hash.select{|k,v| ['ADOB_ID', 'ADR_ID', 'ADS_KEHTIV', 'ADS_OID', 'LAHIAADRESS', 'OLED', 'ORIG_TUNNUS', 'TAISAADRESS', 'VIITEPUNKT_X', 'VIITEPUNKT_Y'].include?(k)}
+      selected_values = row.to_hash.slice(:adob_id, :adr_id, :ads_kehtiv, :ads_oid, :lahiaadress, :oled, :orig_tunnus, :taisaadress, :viitepunkt_x, :viitepunkt_y)
 
-      lower_case_hash = {}
-      selected_values.each_pair do |k,v|
-        lower_case_hash.merge!({k.downcase => v})
-      end
-
-      AdsEntry.create!(lower_case_hash)
+      AdsEntry.create!(selected_values)
     end
     p 'Import done!'
   end
